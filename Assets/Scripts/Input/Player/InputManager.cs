ccceb7efa23f8d9ca16631278c440336d464281d
Reinier Maartense <reiniermaartense@live.nl>
2021-03-22 12:35:23 +0100

    Fixed Camera

---
+++ Assets/Scripts/Input/Player/InputManager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    public static InputManager Instance;
+
+    private PlayerInput playerInput;
+
+    private void Awake()
+    {
+        Instance = this;
+        playerInput = new PlayerInput();
+    }
+
+    private void OnEnable()
+    {
+        playerInput.Enable();
+    }
+
+    private void OnDisable()
+    {
+        playerInput.Disable();
+    }
+
+    public Vector2 GetMovement()
+    {
+        return playerInput.PlayerMain.Move.ReadValue<Vector2>();
+    }
+
+    public Vector2 GetLook()
+    {
+        return playerInput.PlayerMain.Look.ReadValue<Vector2>();
+    }
+}
