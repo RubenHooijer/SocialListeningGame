738fcd54d85df647dd93bba972b27647fc4b109b
reiniermaartense@live.nl <Reinier27>
2021-04-22 15:35:11 +0200

    doorminigame progress

--- Assets/Scripts/Minigames/DoorMinigame/MinigameBar.cs
+++ Assets/Scripts/Minigames/DoorMinigame/MinigameBar.cs
@@ -5,17 +5,68 @@ using UnityEngine.UI;
 
 public class MinigameBar : MonoBehaviour
 {
+    //Components
     private Slider slider;
+    [SerializeField] private Image handleImage;
 
-    [SerializeField] private float speed = 1f;
+    private Color baseColor;
+
+    //Variables
+    [SerializeField] private float baseSpeed = 1f;
+
+    [SerializeField] private float doorOpenValue;
+    [SerializeField] private float doorCurrentValue;
+
+    [SerializeField] private float ClickCooldown = 1;
+
+    private bool canClick;
+
 
     private void Start()
     {
         slider = GetComponent<Slider>();
+
+        canClick = true;
+
+        baseColor = handleImage.color;
+
+        InputManager.Instance.InteractPerformed.AddListener(AddToValue);
     }
 
     private void Update()
-    {   
-        slider.value = Mathf.PingPong(Time.time, 1);
+    {
+        slider.value = Mathf.PingPong(Time.time * baseSpeed, 1);
+
+        //Reset slider
+        if(slider.value <= 0.01f)
+        {
+            canClick = true;
+            handleImage.color = new Color(baseColor.r, baseColor.g, baseColor.b, 1f);
+        }
+    }
+
+    private void AddToValue()
+    {
+        if(!canClick)
+        {
+            return;
+        }
+
+        canClick = false;
+
+        //Grey out slider
+        handleImage.color = new Color(baseColor.r, baseColor.g, baseColor.b, 0.4f);
+
+        //Add value depending on bar value.
+        float value = 0.5f;
+        if(slider.value > 0.5f)
+        {
+            value = 1;
+        }
+        if(slider.value > 0.75f)
+        {
+            value = 2.5f;
+        }
+        doorCurrentValue += value;
     }
 }
