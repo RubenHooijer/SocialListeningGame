022b3c5e9cdfa0f7f47cfff42b3b1d7cf5956c6b
Wouter Kamies <31508042+WouterKamies@users.noreply.github.com>
2021-05-20 17:07:06 +0200

    Added Teeth vfx for worm

---
+++ Assets/Scripts/MaterialLerp.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialLerp : MonoBehaviour
+{
+    public Renderer Oorwurm;
+    public bool showTeeth;
+    private float lerp;
+    private float t = 0.0f;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        Oorwurm = Oorwurm.GetComponent<Renderer>();
+        showTeeth = false;
+    }
+
+    public void StartMat()
+    {
+        showTeeth = true;
+    }
+
+    void Update()
+    {
+        if (t <= 1 && showTeeth)
+        {
+            lerp = Mathf.Lerp(0, 1, t);
+            t += speed * Time.deltaTime;
+            Oorwurm.material.SetFloat("_Tand", lerp);
+        }
+        
+    }
+
+
+}
