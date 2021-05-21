37ff017a20e0cc5ec5bbedcd5c3378904af0318d
Ruben Hooijer <ruben.hooijer@littlechicken.nl>
2021-05-21 13:11:57 +0200

    Dialogue scene 3

--- Assets/SiccityDialogue/Scripts/Nodes/ProgressionEvent.cs
+++ Assets/SiccityDialogue/Scripts/Nodes/ProgressionEvent.cs
@@ -1,4 +1,6 @@
-﻿namespace Dialogue {
+﻿using XNode;
+
+namespace Dialogue {
 
     [NodeTint("#737C58")][CreateNodeMenu("Progression Event", order = 6)]
 	public class ProgressionEvent : DialogueBaseNode {
@@ -21,6 +23,16 @@
 					break;
 				default:
 					break;
+			}
+
+			Next();
+		}
+
+		public void Next() {
+			NodePort port = GetOutputPort("output");
+			for (int i = 0; i < port.ConnectionCount; i++) {
+				NodePort connection = port.GetConnection(i);
+				(connection.node as DialogueBaseNode).Trigger();
 			}
 		}
 
