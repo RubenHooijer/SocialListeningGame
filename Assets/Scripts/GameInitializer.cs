292714c9239cd3bba2a0ff0fccda16c733f0580d
Ruben Hooijer <ruben.hooijer@littlechicken.nl>
2021-05-26 12:08:01 +0200

    Add ingame debug console

--- Assets/Scripts/GameInitializer.cs
+++ Assets/Scripts/GameInitializer.cs
@@ -4,7 +4,8 @@ using UnityEngine.SceneManagement;
 
 public class GameInitializer : MonoBehaviour {
 
-    [SerializeField, SceneName] private string[] startingScenes;
+    [SerializeField, SceneName] private string mainScene;
+    [SerializeField, SceneName] private string[] additiveScenes;
     [SerializeField] private GameController gameController;
 
     private void Awake() {
@@ -12,12 +13,19 @@ public class GameInitializer : MonoBehaviour {
     }
 
     private IEnumerator SceneLoadingRoutine() {
-        for (int i = 0; i < startingScenes.Length; i++) {
-            yield return SceneManager.LoadSceneAsync(startingScenes[i], LoadSceneMode.Additive);
+        for (int i = 0; i < additiveScenes.Length; i++) {
+            yield return SceneManager.LoadSceneAsync(additiveScenes[i], LoadSceneMode.Additive);
         }
+
+        SceneManager.sceneLoaded += OnSceneLoaded;
+        SceneManager.LoadSceneAsync(mainScene, LoadSceneMode.Additive);
 
         SceneManager.UnloadSceneAsync("Loader");
         Instantiate(gameController);
     }
 
+    private void OnSceneLoaded(Scene scene, LoadSceneMode loadSceneMode) {
+        SceneManager.SetActiveScene(scene);
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
 }
